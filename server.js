360b119d23196c33d6a8d612e06541c65fd942f8
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-04 17:30:25 +0100

    initial express setup.

---
+++ server.js
@@ -0,0 +1,19 @@
+import app from './app';
+import mongoose from 'mongoose';
+
+const { PORT, DB_URI } = process.env;
+
+(async () => {
+  try {
+    await mongoose.connect(DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('connected to db');
+  } catch (e) {
+    console.log(e);
+  }
+})();
+
+app.listen(PORT, () => console.log('running on localhost:3001'));
