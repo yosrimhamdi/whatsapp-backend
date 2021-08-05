98566d7fa5d922de0c799372ac977a3a23fc425f
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-05 14:50:22 +0100

    accepting data from the request body.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import messages from './routes/messages';
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/v1/messages', messages);
 
 export default app;
--- controllers/addMessage.js
+++ controllers/addMessage.js
@@ -1,9 +1,7 @@
 import Message from '../models/Message';
 
 export default async (req, res) => {
-  const message = await Message.create({
-    content: 'hi theres',
-  });
+  const message = await Message.create(req.body);
 
   res.status(201).json({ status: 'success', data: { message } });
 };
