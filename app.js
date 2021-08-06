17a8fa7fcee36a136b1fbed9c22e4e83225afb51
yosrimhamdi <yosrimhamdi@outlook.com>
2021-08-06 14:29:07 +0100

    added an express middlware.

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.json());
+
 app.use('/api/v1/messages', messages);
 
 export default app;
--- controllers/getMessages.js
+++ controllers/getMessages.js
@@ -3,5 +3,5 @@ import Message from '../models/Message';
 export default async (req, res) => {
   const messages = await Message.find();
 
-  res.status(200).json({ status: 'success', data: { messages } });
+  res.status(200).json({ status: 'success', messages });
 };
